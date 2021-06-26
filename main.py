9c8f660496cf2bcfef53deeffa086ecc353349a2
Manya shree <84006140+manyavangimalla@users.noreply.github.com>
2021-06-26 10:57:24 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# This arithmetic_arrangerentrypoint file to be used in development. Start by reading README.md
+#  import arithmetic_arranger
+from unittest import main
+
+
+print((["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
